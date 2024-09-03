ff08ebe637322f2b13a7e2506474b5d8efaf5bcc
manik <maniksarker652@gmail.com>
2024-09-03 10:41:23 +0600

    make some changes in auth schema

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,9 +1,10 @@
 const { z } = require("zod");
 
-const userValidationSchema = z.object({
+const authValidationSchema = z.object({
   fullName: z.string().min(1, "Full name is required"),
   email: z.string().email("Invalid email address").min(1, "Email is required"),
   password: z.string().min(1, "Password is required"),
 });
 
-module.exports = userValidationSchema;
+
+module.exports = authValidationSchema;
