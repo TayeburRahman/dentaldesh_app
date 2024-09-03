5941f9393be1bbe5e0d2698269c07d084fa7dc42
manik <maniksarker652@gmail.com>
2024-09-03 10:38:10 +0600

    create validation schema for user

---
+++ src/modules/user/user.validation.js
@@ -0,0 +1,9 @@
+const { z } = require("zod");
+
+const userValidationSchema = z.object({
+  fullName: z.string().min(1, "Full name is required"),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(1, "Password is required"),
+});
+
+module.exports = userValidationSchema;
